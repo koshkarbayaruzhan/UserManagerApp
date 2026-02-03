ed8d8aca23a17b522ca781893ee635dd6d7f2d11
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-02-04 00:47:05 +0500

    Update User.java

--- src/User.java
+++ src/User.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
-
 public class User {
+    private String name;
+    private String email;
+    private String role;
+
+    public User(String name, String email, String role) {
+        this.name = name;
+        this.email = email;
+        this.role = role;
+    }
+    public String getEmail() { return email; }
+    public void setName(String name) { this.name = name; }
+    public void setRole(String role) { this.role = role; }
+
+    @Override
+    public String toString() {
+        return "User{name='" + name + "', email='" + email + "', role='" + role + "'}";
+    }
 }
